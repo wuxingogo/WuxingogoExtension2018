1ff3c40be200670e4b124f604f84746f47befc3c
Wuxingogo <895821859@qq.com>
2015-11-27 09:46:18 +0800

    test debug log

---
+++ WuxingogoRuntime/TestBehavior/XDebugLog.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using wuxingogo.Runtime;
+
+namespace XBehaviorRunntime{
+	public class XDebugLog : XBehaviorAction {
+		
+		public string info = "";
+		public override void OnStart()
+		{
+			base.OnStart();
+			Debug.Log(info);
+		}
+	}
+	
+}
