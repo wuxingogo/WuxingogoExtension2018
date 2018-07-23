f782bba4b45f47e3dd479b7a81896c1001156c98
Wuxingogo <52111314ly@gmail.com>
2018-07-23 17:05:30 +0800

    reflection

--- WuxingogoRuntime/Tools/XReflectionUtils.cs
+++ WuxingogoRuntime/Tools/XReflectionUtils.cs
@@ -421,5 +421,30 @@ namespace wuxingogo.Reflection
 
 			}
 		}
+		
+		public static Component CopyComponent(Component original, GameObject destination) 
+		{ 
+			System.Type type = original.GetType(); 
+			Component copy = destination.AddComponent(type); 
+			// Copied fields can be restricted with BindingFlags 
+			System.Reflection.FieldInfo[] fields = type.GetFields();  
+			foreach (System.Reflection.FieldInfo field in fields) 
+			{ 
+				field.SetValue(copy, field.GetValue(original)); 
+			} 
+			return copy; 
+		} 
+     
+		public static T CopyComponent<T>(T original, GameObject destination) where T : Component 
+		{ 
+			System.Type type = original.GetType(); 
+			Component copy = destination.AddComponent(type); 
+			System.Reflection.FieldInfo[] fields = type.GetFields(); 
+			foreach (System.Reflection.FieldInfo field in fields) 
+			{ 
+				field.SetValue(copy, field.GetValue(original)); 
+			} 
+			return copy as T; 
+		} 
     }
 }
